74ff19cacc530aec2ac103bd48a2c04daa6d20a9
andrijaweb <andrijawebd@gmail.com>
2024-09-26 15:19:09 +0200

    Added sorting algorithms folder, added bubble sort algorithm

---
+++ src/sorting-algorithms/bubbleSort/bubbleSort.js
@@ -0,0 +1,11 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([333, 12, 2, 999, 214, 5555, 111]));

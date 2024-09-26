6df37630d2b92267c33e314586ab143d6aa61de5
andrijaweb <andrijawebd@gmail.com>
2024-09-26 15:24:37 +0200

    Optimized bubble sort with no swapping method

--- src/sorting-algorithms/bubbleSort/bubbleSort.js
+++ src/sorting-algorithms/bubbleSort/bubbleSort.js
@@ -8,4 +8,26 @@ function bubbleSort(arr) {
   return arr;
 }
 
+// Optimized bubble sort
+function bubbleSortOptimized(arr) {
+  let noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+
+        noSwaps = false;
+      }
+    }
+
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
+
 console.log(bubbleSort([333, 12, 2, 999, 214, 5555, 111]));
+console.log(bubbleSortOptimized([333, 12, 2, 999, 214, 5555, 111]));

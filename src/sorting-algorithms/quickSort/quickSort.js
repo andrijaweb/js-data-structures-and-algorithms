cb4bfd5e1ac9bdd90a2d4e664b721e01f39d18b5
andrijaweb <andrijawebd@gmail.com>
2024-09-28 23:04:00 +0200

    Updated quicksort algorithm (added actual quickSort func)

--- src/sorting-algorithms/quickSort/quickSort.js
+++ src/sorting-algorithms/quickSort/quickSort.js
@@ -14,4 +14,14 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIndex;
 }
 
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+
+  return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));

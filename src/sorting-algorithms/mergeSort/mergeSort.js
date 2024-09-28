b7e6be875b575ba419bd79fad4d06f1db6f2d15f
andrijaweb <andrijawebd@gmail.com>
2024-09-28 20:01:10 +0200

    Updated mergeSort algorithm

--- src/sorting-algorithms/mergeSort/mergeSort.js
+++ src/sorting-algorithms/mergeSort/mergeSort.js
@@ -20,4 +20,14 @@ function mergeArrays(arr1, arr2) {
   return results;
 }
 
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const arr1 = mergeSort(arr.slice(0, middle));
+  const arr2 = mergeSort(arr.slice(middle));
+  return mergeArrays(arr1, arr2);
+}
+
 console.log(mergeArrays([1, 10, 50], [2, 14, 99, 100]));
+console.log(mergeSort([2, 58, 4, 99, 5]));

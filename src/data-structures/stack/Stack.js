d1e5c56d05c0e86dd0af6be2a4b3c63f500bd93a
andrijaweb <andrijawebd@gmail.com>
2024-10-04 21:00:13 +0200

    Added push method

--- src/data-structures/stack/Stack.js
+++ src/data-structures/stack/Stack.js
@@ -11,4 +11,27 @@ class Stack {
     this.last = null;
     this.len = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) this.first = this.last = newNode;
+    else {
+      const curFirst = this.first;
+      this.first = newNode;
+      this.first.next = curFirst;
+    }
+
+    return ++this.len;
+  }
 }
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(6);
+stack.push(3);
+stack.push(67);
+stack.push(33);
+stack.push(21);
+
+console.log(stack);

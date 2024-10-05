900122de5f8ac18ecf071842da27c031ed7d9860
andrijaweb <andrijawebd@gmail.com>
2024-10-05 20:04:24 +0200

    Added insert method

--- src/data-structures/binary-search-tree/BinarySearchTree.js
+++ src/data-structures/binary-search-tree/BinarySearchTree.js
@@ -10,6 +10,35 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (val === current.val) return undefined;
+
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();

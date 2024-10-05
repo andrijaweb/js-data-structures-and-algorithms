5cb2a73ec609dc95a9f83650e71e55a5c9362450
andrijaweb <andrijawebd@gmail.com>
2024-10-05 20:04:05 +0200

    Added binary search tree folder, added base classes

---
+++ src/data-structures/binary-search-tree/BinarySearchTree.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const tree = new BinarySearchTree();
+
+console.log(tree);

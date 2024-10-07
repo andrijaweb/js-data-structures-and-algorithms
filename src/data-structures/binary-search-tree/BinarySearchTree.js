f296d66b46deaeaf1e0a2e1ddae62dc3b54cb36f
andrijaweb <andrijawebd@gmail.com>
2024-10-07 23:25:45 +0200

    Added DFSInOrder method

--- src/data-structures/binary-search-tree/BinarySearchTree.js
+++ src/data-structures/binary-search-tree/BinarySearchTree.js
@@ -95,6 +95,19 @@ class BinarySearchTree {
 
     return visited;
   }
+
+  DFSInOrder() {
+    const data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+
+    return data;
+  }
 }
 
 const tree = new BinarySearchTree();

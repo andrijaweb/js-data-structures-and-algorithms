76deec8992515e7bbdf8bb65f94756e141f0826c
andrijaweb <andrijawebd@gmail.com>
2024-10-10 22:42:26 +0200

    Added insert method, bubbleUp

--- src/data-structures/binary-heaps/BinaryHeap.js
+++ src/data-structures/binary-heaps/BinaryHeap.js
@@ -2,8 +2,36 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const el = this.values[index];
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parentEl = this.values[parentIndex];
+
+      if (el <= parentEl) break;
+      this.values[parentIndex] = el;
+      this.values[index] = parentEl;
+
+      index = parentIndex;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
 
 console.log(heap.values);

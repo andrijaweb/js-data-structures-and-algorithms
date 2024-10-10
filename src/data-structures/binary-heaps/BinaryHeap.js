70d052ccc94fa32da22413815d8c6331c3388819
andrijaweb <andrijawebd@gmail.com>
2024-10-10 22:44:10 +0200

    Added extractMax method, bubbleDown

--- src/data-structures/binary-heaps/BinaryHeap.js
+++ src/data-structures/binary-heaps/BinaryHeap.js
@@ -23,6 +23,51 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+
+    return max;
+  }
+
+  bubbleDown() {
+    let index = 0;
+    const length = this.values.length;
+    const el = this.values[0];
+
+    while (1) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > el) swap = leftChildIndex;
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (!swap && rightChild > el) ||
+          (swap !== null) & (rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (!swap) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = el;
+      index = swap;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
